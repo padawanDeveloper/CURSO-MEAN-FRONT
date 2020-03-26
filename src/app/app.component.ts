02c8386fe664f6b37917bdeed897fbbf09fa64e4
Esteban Ortiz <ematiasortiz@gmail.com>
2020-03-26 00:41:22 -0300

    modificado app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from "@angular/core";
+import { UserService } from "./services/user.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  providers: [UserService]
 })
-export class AppComponent {
-  title = 'front-curso-mean';
+export class AppComponent implements OnInit, DoCheck {
+  public title = "MEAN-SOCIAL";
+  public identity;
+  constructor(private _userService: UserService) {
+    this.title = "Curso MEAN";
+  }
+
+  ngOnInit() {
+    this.identity = this._userService.getToken();
+  }
+
+  ngDoCheck() {
+    this.identity = this._userService.getToken();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { appRoutingProvider, routing } from "./app.routing";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, routing],
+  providers: [appRoutingProvider],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
